f0a591469cb7fedc5b9613ce5cde5c3540583688
Frank McDermott <fmcdermott4@gmail.com>
2021-05-12 08:41:49 -0400

    commiting before editing

---
+++ index.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'managerName',
+        message: "Who is your project manager?"
+    },
+    {
+        type: 'input',
+        name: 'managerId',
+        message: "What is your manager's employee ID?"
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "What is your manager's email?"
+    },
+    {
+        type: 'input', 
+        name: 'managerNumber',
+        message: "What is your manager's email?"
+    }
+]
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: "What is your engineer's name?"
+    },
+    {
+        type: 'input',
+        name: 'engineerId',
+        message: "What is your engineer's employee ID?"
+    },
+    {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is your engineer's email?"
+    },
+    {
+        type: 'input', 
+        name: 'engineerGitHub',
+        message: "What is your engineer's GitHub username?"
+    }
+]
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'internName',
+        message: "What is your intern's name?"
+    },
+    {
+        type: 'input',
+        name: 'internId',
+        message: "What is your intern's employee ID?"
+    },
+    {
+        type: "input",
+        name: "internEmail",
+        message: "What is your intern's email?"
+    },
+    {
+        type: 'input', 
+        name: 'internSchool',
+        message: "What is your intern's school?"
+    }
+]
